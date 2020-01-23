75b059698ce20acc78a89714a6a0f411cbfd0fb8
Hasanuzzaman <shuvo009@gmail.com>
2020-01-23 18:26:01 +0600

    base controller

--- src/apiRoute/baseController.ts
+++ src/apiRoute/baseController.ts
@@ -1,17 +1,37 @@
 import * as Hapi from "hapi";
+import { IBaseService } from "../interfaces";
+import { IBaseModel } from "../models";
 
-export abstract class BaseController {
+export abstract class BaseController<T extends IBaseModel> {
     constructor(public controllerName: string) {
 
     }
 
-    public initBaseRoutes(server: Hapi.Server): void {
+    public initBaseRoutes(server: Hapi.Server, baseService: IBaseService<T>): void {
         server.route({
-            handler: (request, h) => {
+            handler: (request: Hapi.Server) => {
                 return "Hello World!2";
             },
             method: "GET",
             path: `/api/${this.controllerName}/getall`
         });
     }
+
+    /* #region  Handelars */
+    private async list() {
+
+    }
+
+    private async create() {
+
+    }
+
+    private async update() {
+
+    }
+
+    private async delete() {
+
+    }
+    /* #endregion */
 }
